5f1670df2a2d0077705b591851ba1bc16cffdc11
SlOVVliKE <thirakan.weef64@gmail.com>
2025-12-20 21:03:46 +0700

    edit10

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
         'http://127.0.0.1:5500', 
         'http://localhost:5501', 
         'http://127.0.0.1:5501',
-        'https://gametoword.netlify.app'
+        'https://gamesthaiwords.netlify.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
